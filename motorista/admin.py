5836507d72a4ec9ce6a08a492e3030624583c7b5
joabe levi <joabe-levi@hotmail.com>
2024-04-21 00:26:16 -0300

    - Motorista     - feat: Ajustes admin

--- motorista/admin.py
+++ motorista/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 
+from core.basics.admins.basic_admins import BasicAdmin
 from motorista.models import Motorista
 
-admin.site.register(Motorista)
+
+@admin.register(Motorista)
+class MotoristaAdmin(BasicAdmin):
+    pass
