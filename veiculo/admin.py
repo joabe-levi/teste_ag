641d67f31bc1dc9bfd97424aa2489b622cd667aa
joabe levi <joabe-levi@hotmail.com>
2024-04-21 00:26:27 -0300

    - Veículo     - feat: Ajustes admin

--- veiculo/admin.py
+++ veiculo/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 
+from core.basics.admins.basic_admins import BasicAdmin
 from veiculo.models import Veiculo
 
-admin.site.register(Veiculo)
+
+@admin.register(Veiculo)
+class VeiculoAdmin(BasicAdmin):
+    pass
